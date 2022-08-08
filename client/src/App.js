655284c604cce38a2df154138de29a1868286a32
parasite-e <fardinemonbd@gmail.com>
2022-08-08 13:47:58 +0600

    redux initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { AppBar, Container, Grid, Grow, Typography } from '@material-ui/core';
+import Posts from './components/Posts/Posts.js';
+import Form from './components/Form/Form.js'
+import useStyles from './styles'
+
+import memories from './images/memories.png';
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+        <img className={classes.image} src={memories} alt='memories' height='60' />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xl={12} sm={7}>
+              <Posts></Posts>
+            </Grid>
+            <Grid item xl={12} sm={4}>
+              <Form></Form>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
