4c3f2509e1d0aa051f8e79a01ba8ac490455d87a
parasite-e <fardinemonbd@gmail.com>
2022-08-11 18:17:23 +0600

    delete functionality added

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -7,8 +7,14 @@ import React from 'react';
 import useStyles from './styles'
 import moment from 'moment';
 
+import { useDispatch } from 'react-redux';
+import { deletePost } from '../../../actions/posts';
+
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+
+    const dispatch = useDispatch();
+
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -33,7 +39,7 @@ const Post = ({ post, setCurrentId }) => {
             </CardContent>
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" onClick={() => { }}><ThumbUpAltIcon fontSize="small" /> Like {post.likeCount} </Button>
-                <Button size="small" color="primary" onClick={() => { }}><DeleteIcon fontSize="small" /> Delete</Button>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
             </CardActions>
         </Card>
     );
